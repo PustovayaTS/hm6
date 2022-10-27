4a6648b3e50f97d6505c4e1bec7d3f4592a33967
PustovayaTS <ais_ta@bk.ru>
2022-10-27 20:31:23 +0700

    Работа с исключениями. Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception{
+        String login = "аb_cfghfgh1";
+        String password = "password";
+        String confPassword = "password";
+        Data.checkData(login, password, confPassword);
+        try {
+            Data.checkData(login, password, confPassword);
+        } catch (WrongLoginException e,WrongPasswordException e){
+            System.out.println("Перехват не корректный логин");
+        }
     }
 }
